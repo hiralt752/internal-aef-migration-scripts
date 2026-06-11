f0b7c3edf83a272de73c33cd5819fcb61adf7d9c
RohitJigar <jigar202613@outlook.com>
2026-06-11 12:11:02 +0530

    updated code for fib_dnd

---
+++ transformation_engine/transformers/fib_dnd_transformer.py
@@ -0,0 +1,57 @@
+from builders.metadata_builder import build_metadata
+
+from builders.fib_dnd_itembody_builder import (
+    build_fib_dnd_item_body
+)
+
+from builders.fib_dnd_outcome_builder import (
+    build_dnd_outcome_declaration
+)
+
+
+class FIBDNDTransformer:
+
+    def __init__(self, raw, question_id, lesson):
+
+        self.raw = raw
+        self.question_id = question_id
+        self.lesson = lesson
+
+    def transform(self):
+
+        payload = {
+
+            "schemaVersion": {
+
+                "major": 1,
+                "minor": 0,
+                "patch": 0
+            },
+
+            "type": "DND",
+
+            "subType": "BLANK_ON_QUESTION",
+
+            "metadata":
+                build_metadata(
+                    self.raw
+                ),
+
+            "itemBody":
+                build_fib_dnd_item_body(
+                    self.raw,
+                    self.question_id,
+                    self.lesson
+                ),
+
+            "responseDeclaration": {
+                "maxAttempts": 1
+            },
+
+            "outcomeDeclaration":
+                build_dnd_outcome_declaration(
+                    self.raw
+                )
+        }
+
+        return payload
