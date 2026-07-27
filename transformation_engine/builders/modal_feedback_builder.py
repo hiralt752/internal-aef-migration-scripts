93e2af3bd61d11e5bbb2ba49bb39d661e6cf355a
Vivek20-04 <vrawal4124@gmail.com>
2026-07-27 09:47:41 +0530

    feat: add needHelpStrategy to modal feedback builder

--- transformation_engine/builders/modal_feedback_builder.py
+++ transformation_engine/builders/modal_feedback_builder.py
@@ -58,7 +58,7 @@ def build_modal_feedback(
         }
 
     passage = build_passage(body.get("passage"))
-
+    modal["needHelpStrategy"] ="SCAFFOLDED"   
     if passage:
         modal["passage"] = passage
 
